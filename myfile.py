81968281c5c5aa9b042130d9dc20ad016f7365b5
marcus.vinicius <vinicius.costa@ceuma.br>
2023-05-31 14:01:43 -0300

    feat: adicionando tabs

--- myfile.py
+++ myfile.py
@@ -5,18 +5,40 @@ st.image('logo.png', width=250)
 st.header('Bem-vindo ao nosso sistema!')
 st.text('Faça o upload de uma foto para identificação automatizada de segurança!')
 
-upload_file = st.file_uploader("Escolha uma foto")
+tabImg, tabVideo, tabWebCam = st.tabs(["Imagem", "Vídeo", "Webcam"])
 
 isSecurity = False
 
-if upload_file is not None:
-  with st.spinner('Carregando...'):
-    time.sleep(5)
-
-  st.image(upload_file, width=350)
-
-  if isSecurity: 
+def statusMessage(status):
+  if status: 
     st.success('Todos procedimentos de segurança respeitados!', icon="✅")
   else:
     st.error('Procedimento de segurança burlado!! Atenção!', icon="🚨")
 
+def loading():
+  with st.spinner('Carregando...'):
+      time.sleep(5)
+
+with tabImg:
+  upload_file = st.file_uploader("Escolha uma foto")
+
+  if upload_file is not None:
+    loading()
+
+    st.image(upload_file, width=350)
+    statusMessage(True)
+
+with tabVideo:
+  upload_file = st.file_uploader("Escolha um vídeo")
+
+  if upload_file is not None:
+    loading()
+    video_bytes = upload_file.read()
+    st.video(video_bytes)
+
+with tabWebCam:
+  picture = st.camera_input("Tire uma foto")
+
+  if picture:
+    loading()
+    st.image(picture)
